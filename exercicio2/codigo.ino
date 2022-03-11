cda9b6eb44b0a24125e8ebce363b0c1380f328ed
megaboltgames <99339347+megaboltgames@users.noreply.github.com>
2022-03-11 11:22:48 -0300

    Create codigo.ino

---
+++ exercicio2/codigo.ino
@@ -0,0 +1,17 @@
+void setup()
+{
+  pinMode(1, OUTPUT);
+  pinMode(2, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(1, HIGH);
+  delay(1000);
+  digitalWrite(1, LOW);
+  delay(1000);
+  digitalWrite(2, HIGH);
+  delay(1000);
+  digitalWrite(2, LOW);
+  delay(1000);
+}
