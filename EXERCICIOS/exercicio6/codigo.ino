7d5bf87f1bb3cdb7e7a435845eb4370e2089bedb
megaboltgames <99339347+megaboltgames@users.noreply.github.com>
2022-03-14 08:37:11 -0300

    Update codigo.ino

--- EXERCICIOS/exercicio6/codigo.ino
+++ EXERCICIOS/exercicio6/codigo.ino
@@ -1,54 +1,41 @@
-const int botao1 = 3;
-const int botao2 = 4;
-const int botao3 = 5;
-const int botao4 = 6;
-const int botao5 = 7;
-
 int var1;
 int var2;
 int var3;
 int var4;
 int var5;
+  
+void setup()
+{
+  pinMode(1, INPUT);
+  pinMode(2, INPUT);
+  pinMode(3, INPUT);
+  pinMode(4, INPUT);
+  pinMode(5, INPUT);
+  Serial.begin(9600);
+}
 
- void setup()
- { 
-      pinMode(botao1, INPUT);
-      pinMode(botao2, INPUT);
-      pinMode(botao3, INPUT); 
-      pinMode(botao4, INPUT); 
-      pinMode(botao5, INPUT); 
-      Serial.begin(9600);
- }
- 
-
-
- void loop()
- {
-    
-var1 = digitalRead(botao1);
-var2 = digitalRead(botao2);
-var3 = digitalRead(botao3);
-var4 = digitalRead(botao4);
-var5 = digitalRead(botao5);
-   //Serial.println(var1);
-
- if (var1){
-   Serial.println("bem 10");
- }
-
- if (var2){
-   Serial.println("viva a estetica");
- }
-
- if (var3){
-   Serial.println("frede");
- }
-
- if (var4){
-   Serial.println("selvagem");
- }
-
- if (var5){
-   Serial.println("simbolico");
- }
- }
+void loop()
+{
+  var1 = digitalRead(1);
+  var2 = digitalRead(2);
+  var3 = digitalRead(3);
+  var4 = digitalRead(4);
+  var5 = digitalRead(5);
+  
+  if(var1){
+  Serial.println("a");
+  }
+  if(var2){
+  Serial.println("aa");
+  }
+  if(var3){
+  Serial.println("aaa");
+  }
+  if(var4){
+  Serial.println("aaaa");
+  }
+  if(var5){
+  Serial.println("aaaaa");
+  }
+  delay(10);
+}
