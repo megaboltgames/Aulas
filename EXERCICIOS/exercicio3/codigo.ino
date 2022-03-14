1fef7e7dd3db78f662743ada01b046f34356f58d
megaboltgames <99339347+megaboltgames@users.noreply.github.com>
2022-03-14 08:39:03 -0300

    Update codigo.ino

--- EXERCICIOS/exercicio3/codigo.ino
+++ EXERCICIOS/exercicio3/codigo.ino
@@ -1,44 +1,33 @@
-const int led1 = 6;
-const int led2 = 7;
-const int led3 = 8;
-const int led4 = 9;
-const int led5 = 10;
+void setup()
+{
+  pinMode(1, OUTPUT);
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5,OUTPUT);
+}
 
-
- void setup()
- { 
-
-      pinMode(led1, OUTPUT);
-      pinMode(led2, OUTPUT);
-      pinMode(led3, OUTPUT);
-      pinMode(led4, OUTPUT);
-      pinMode(led5, OUTPUT);
-       //Serial.begin(9600);
- }
- 
-
- void loop()
- {
+void loop()
+{
+  digitalWrite(1, HIGH);
+  delay(2000);
+  digitalWrite(2, HIGH);
+  delay(2000);
+  digitalWrite(3, HIGH);
+  delay(2000);
+  digitalWrite(4, HIGH);
+  delay(2000);
+  digitalWrite(5, HIGH);
+  delay(2000);
+  digitalWrite(5, LOW);
+  delay(2000);
+  digitalWrite(4, LOW);
+  delay(2000);
+  digitalWrite(3, LOW);
+  delay(2000);
+  digitalWrite(2, LOW);
+  delay(2000);
+  digitalWrite(1, LOW);
+  delay(2000);
   
-       digitalWrite(led1, HIGH);
-       delay(2000);
-
-       digitalWrite(led2, HIGH);
-       delay(2000);
-
-       digitalWrite(led3, HIGH);
-       delay(2000);
-
-       digitalWrite(led4, HIGH);
-       delay(2000);
-
-       digitalWrite(led5, HIGH);
-       delay(2000);
-
-       digitalWrite(led1, LOW);
-       digitalWrite(led2, LOW);
-       digitalWrite(led3, LOW);
-       digitalWrite(led4, LOW);
-       digitalWrite(led5, LOW);
-       delay(2000);
- }
+}
